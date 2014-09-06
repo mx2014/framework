88988826d5e4d51bc5c401a9e7ca9b5f4894fc00
rayln <rayln1983@gmail.com>
2014-09-06 16:32:43 +0800

    init

---
+++ framework_maven/src/main/java/com/mx/util/SqlUtil.java
@@ -0,0 +1,18 @@
+package com.mx.util;
+
+import java.util.Properties;
+
+public class SqlUtil {
+
+	private Properties properties;
+
+	public Properties getProperties() {
+		return properties;
+	}
+
+	public void setProperties(Properties properties) {
+		this.properties = properties;
+	}
+	
+	
+}
